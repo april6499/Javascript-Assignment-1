837281473172c6f398d2df12f6c14327acbf644f
Sheetal Kumari <107206434+april6499@users.noreply.github.com>
2025-10-25 16:33:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+// Question 1
+let a=2;
+let b=5;
+let res=a+b;
+console.log(res);//output 7
+
+//Question 2
+let a1=5;
+let b1=3;
+if(a1>b1){
+    console.log("true");
+
+}
+else{
+    console.log("false");
+}
+
+//Question 3
+let A = 12;
+let B = 20;
+
+if (A % 10 === 0 && B % 10 === 0) {
+  console.log("true");
+}
+else if (A % 10 !== 0 && B % 10 !== 0) {
+  console.log("false");
+}
+else if (A % 10 === 0 || B % 10 === 0) {
+  console.log("true");
+}
+//Question 4
+let number=4567;
+    console.log(number.toString()[0])
+  
+
+//Question 5
+let x=4567;
+let result=x%10;
+console.log(result);
+
+//Question 6
+let i=2;
+let j=9;
+let k;
+ k=j%i;
+    console.log(k);
+
+//Question 7
+
+let p=2;
+let q=5;
+let r=p*q;
+console.log(r);
+
+//Question 8
+let l=50;
+let m=20;
+let n=100;
+
+let sum;
+sum=l+m+n;
+average=sum/3;
+console.log(sum);
+console.log(average);
+
+
+
+
+
+
+
+
+
+
+
+
