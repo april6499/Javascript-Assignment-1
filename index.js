cbd26e868914fa9f52e4eb75f990e79ef4c03b75
Sheetal Kumari <107206434+april6499@users.noreply.github.com>
2025-10-25 16:44:15 +0530

    index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ let res=a+b;
 console.log(res);//output 7
 
 //Question 2
-let a1=5;
-let b1=3;
-if(a1>b1){
-    console.log("true");
+let c=5;
+let d=3;
+if(c>d){
+    console.log("true");//output true
 
 }
 else{
@@ -26,31 +26,31 @@ else if (A % 10 !== 0 && B % 10 !== 0) {
   console.log("false");
 }
 else if (A % 10 === 0 || B % 10 === 0) {
-  console.log("true");
+  console.log("true");//output true
 }
 //Question 4
 let number=4567;
-    console.log(number.toString()[0])
+    console.log(number.toString()[0])//output 4
   
 
 //Question 5
 let x=4567;
 let result=x%10;
-console.log(result);
+console.log(result);//output 7
 
 //Question 6
 let i=2;
 let j=9;
 let k;
  k=j%i;
-    console.log(k);
+    console.log(k);//output 1
 
 //Question 7
 
 let p=2;
 let q=5;
 let r=p*q;
-console.log(r);
+console.log(r);//output 10
 
 //Question 8
 let l=50;
@@ -60,8 +60,8 @@ let n=100;
 let sum;
 sum=l+m+n;
 average=sum/3;
-console.log(sum);
-console.log(average);
+console.log(sum);//output 170
+console.log(average);//output 56.66
 
 
 
